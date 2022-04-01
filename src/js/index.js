1e19f445c6a6d42e18d4955150e5ebc2c3f55d85
Leonardo Garcia <leolgp@gmail.com>
2022-04-01 12:17:28 -0300

    resolvi dados

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,22 @@ const btn = document.getElementById('btn-dice')
 btn.addEventListener('click', jogarDado)
 
 function jogarDado(){
-        let numero = mover(6);
-        console.log('funçao jogarDado: ',numero);
-         document.getElementById('demo').innerHTML = `<img src='src/imagem/dado${numero}.png'>`
+        mover(6)
+        console.log(x);
+        document.getElementById('demo').innerHTML = `<img src='src/imagem/dado${x}.png'>`
 }
-
 
 
 function mover(number){
-        
+     
         let numeroAleatorio = (Math.floor(Math.random()*(number+1)));
-        console.log('funçao mover: ', numeroAleatorio);
-        if (numeroAleatorio === 0 ){
-         jogarDado();
-        } else {
-               return numeroAleatorio 
+        if( numeroAleatorio !== 0){
+                        x = numeroAleatorio
+        } else{
+                mover(number)
         }
-}
+    
+               return x 
+        }
+
 
